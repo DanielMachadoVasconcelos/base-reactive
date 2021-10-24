bd381bd6f5c0f8666c0b1cd1491b53a5f3e6291f
Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>
2021-10-24 17:13:14 +0200

    Added the modules sales, shipment, warehouse, integration test and common (WIP)

---
+++ shipment/src/test/java/base/shipment/ShipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package base.shipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ warehouse/src/test/java/base/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package base.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
