48de3092376073dbdae8cc6062cc2e50d7207dfb
Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>
2021-10-28 20:58:19 +0200

    Added the modules sales, shipment, warehouse, integration test and common (WIP)

--- integration-test/src/test/java/br/com/ead/base/Application.java
+++ integration-test/src/test/java/br/com/ead/base/Application.java
@@ -1,39 +1,41 @@
 package br.com.ead.base;
 
-import lombok.extern.slf4j.Slf4j;
-import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns;
-import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
-import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
-
-import org.junit.platform.engine.FilterResult;
-import org.junit.platform.engine.TestDescriptor;
+import org.junit.platform.engine.discovery.ClassNameFilter;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
 import org.junit.platform.launcher.Launcher;
-import org.junit.platform.launcher.LauncherDiscoveryListener;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
-import org.junit.platform.launcher.LauncherSession;
-import org.junit.platform.launcher.LauncherSessionListener;
-import org.junit.platform.launcher.PostDiscoveryFilter;
-import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestPlan;
 import org.junit.platform.launcher.core.LauncherConfig;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
-import org.junit.platform.reporting.legacy.xml.LegacyXmlReportGeneratingListener;
 
-@Slf4j
+import java.io.PrintWriter;
+
+import static org.junit.platform.engine.discovery.ClassNameFilter.STANDARD_INCLUDE_PATTERN;
+
 public class Application {
 
     public static void main(String[] args) {
 
         LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
-                .selectors(selectPackage("com.example.mytests"))
-                .filters(includeClassNamePatterns(".*Tests"))
+                .selectors(DiscoverySelectors.selectPackage("br.com.ead.base"))
+                .filters(ClassNameFilter.includeClassNamePatterns(STANDARD_INCLUDE_PATTERN))
                 .build();
 
-        try (LauncherSession session = LauncherFactory.openSession()) {
-            TestPlan testPlan = session.getLauncher().discover(request);
-        }
+        LauncherConfig launcherConfig = LauncherConfig.builder()
+                .enableTestEngineAutoRegistration(false)
+                .enablePostDiscoveryFilterAutoRegistration(false)
+                .enableTestExecutionListenerAutoRegistration(false)
+                .build();
+
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+        Launcher launcher = LauncherFactory.create(launcherConfig);
+        TestPlan plan = launcher.discover(request);
+        launcher.execute(plan, listener);
+
+        TestExecutionSummary summary = listener.getSummary();
+        summary.printTo(new PrintWriter(System.out));
     }
 }
---
+++ integration-test/src/test/java/br/com/ead/base/SalesTest.java
@@ -0,0 +1,17 @@
+package br.com.ead.base;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SalesTest {
+
+    @Test
+    public void shouldDoSomethingWhenOrderPlaced() {
+        Assertions.assertTrue(false);
+    }
+
+    @Test
+    public void shouldDoSomethingWhenOrderCancelled() {
+        Assertions.assertTrue(false);
+    }
+}
